f1d3538e10a9fd943be699ecc657fc0abe33e619
Douglas Bastos <dbastos.dev@gmail.com>
2024-03-08 08:50:53 +0000

    adiciona scripts e configuracoes de analise estatica.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tab news de ponta a ponta
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Bom dia Layd! Amo você.</h1>
+function Home() {
+  return <h1>Bom dia Layd! Amo você.</h1>;
 }
 
 export default Home;
